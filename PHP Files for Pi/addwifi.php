130dc0b2e98a362fa8fd636869ce4a6010290136
jaffamd <39970268+jaffamd@users.noreply.github.com>
2018-07-19 17:13:23 -0400

    Update addwifi.php

--- PHP Files for Pi/addwifi.php
+++ PHP Files for Pi/addwifi.php
@@ -1,4 +1,53 @@
-<!DOCTYPE html>
+<?php
+  $ssid = $psk = "";
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["ssid"])) {
+      $nameErr = "Network name is required";
+    } else {
+      $ssid = test_input($_POST["ssid"]);
+    }
+
+    if (empty($_POST["psk"])) {
+      $nameErr = "Password is required";
+    } else {
+      $psk = test_input($_POST["psk"]);
+    }
+
+    if (($ssid != "") && ($psk != "")) {
+      $file = fopen("/etc/wpa_supplicant/wpa_supplicant.conf", "r") or die("Unable to open wpa_supplicant.conf");
+      
+      $x=1;
+      $lastAP = "";
+
+      while (!feof($file)) {
+        $line = fgets($file);
+        if (strpos($line, "id_str") !== false) {
+          $lastAP = "AP" . $x;
+          $x++;
+        }
+      }
+      fclose($file);
+
+      $file = fopen("/etc/wpa_supplicant/wpa_supplicant.conf", "a") or die("Unable to open wpa_supplicant.conf");
+      $strtowrite = PHP_EOL . 'network={' . PHP_EOL . '    ssid="' . $psk . '"' . PHP_EOL . '    psk="' . $psk . '"' . PHP_EOL . '    id_str="AP' . $x . '"' . PHP_EOL . '}';
+      fwrite($file, $strtowrite) or die("Unable to write file!");
+      fclose($file);
+      
+      $file = fopen("/etc/network/interfaces", "a") or die('Unable to open network//interfaces');
+      $strtowritenetwork = PHP_EOL . 'iface AP' . $x . ' inet dhcp';
+      fclose($file);
+      
+      echo 'New WiFi Network Added!';
+      echo 'Please reboot Streambox now';
+  }
+  
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+?>
 <html>
 <head>
   <meta charset="utf-8">
@@ -9,57 +58,6 @@
 </head>
 
 <body>
-  <?php
-    $ssid = $psk = "";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      if (empty($_POST["ssid"])) {
-        $nameErr = "Network name is required";
-      } else {
-        $ssid = test_input($_POST["ssid"]);
-      }
-  
-      if (empty($_POST["psk"])) {
-        $nameErr = "Password is required";
-      } else {
-        $psk = test_input($_POST["psk"]);
-      }
-
-      if (($ssid != "") && ($psk != "")) {
-        $file = fopen("/etc/wpa_supplicant/wpa_supplicant.conf", "r") or die("Unable to open wpa_supplicant.conf");
-        
-        $x=1;
-        $lastAP = "";
-
-        while (!feof($file)) {
-          $line = fgets($file);
-          if (strpos($line, "id_str") !== false) {
-            $lastAP = "AP" . $x;
-            $x++;
-          }
-        }
-        fclose($file);
-
-        $file = fopen("/etc/wpa_supplicant/wpa_supplicant.conf", "a") or die("Unable to open wpa_supplicant.conf");
-        $strtowrite = PHP_EOL . 'network={' . PHP_EOL . '    ssid="' . $psk . '"' . PHP_EOL . '    psk="' . $psk . '"' . PHP_EOL . '    id_str="AP' . $x . '"' . PHP_EOL . '}';
-        fwrite($file, $strtowrite) or die("Unable to write file!");
-        fclose($file);
-        
-        $file = fopen("/etc/network/interfaces", "a") or die('Unable to open network//interfaces');
-        $strtowritenetwork = PHP_EOL . 'iface AP' . $x . ' inet dhcp';
-        fclose($file);
-        
-        echo 'New WiFi Network Added!';
-        echo 'Please reboot Streambox now';
-    }
-    
-    function test_input($data) {
-      $data = trim($data);
-      $data = stripslashes($data);
-      $data = htmlspecialchars($data);
-      return $data;
-    }
-  ?>
-  
   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
   WiFi Network Name: <input type="text" name="ssid"><span class="error">* <?php echo $nameErr;?></span><br><br>
   WiFi Network Password: <input type="text" name="psk"><span class="error">* <?php echo $nameErr;?></span><br><br>
