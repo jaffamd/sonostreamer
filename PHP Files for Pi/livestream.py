c13e98fd95f48b3631cac039c3fdac06c3b20ec8
Elias Jaffa <39970268+jaffamd@users.noreply.github.com>
2018-09-28 00:20:38 -0400

    Delete livestream.py

--- PHP Files for Pi/livestream.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import os
-# Set input to S-video
-# Comment out or use -i 0 for composite video
-os.system('v4l2-ctl -d /dev/video0 -i 1')
-os.system('ffmpeg -y -re -f video4linux2 -standard NTSC -s 720x480 -i /dev/video0 -c:v h264_omx -an -f flv rtmp://yourwebsitehere.com/livestream')
