bb0c336500287c2edc2bf6cd1900e39c8cc5063d
Elias Jaffa <39970268+jaffamd@users.noreply.github.com>
2020-06-26 00:43:49 -0400

    Yet more attempts to properly trigger autohotspot script

--- app.js
+++ app.js
@@ -67,23 +67,23 @@ function apply(settings) {
 }
 
 function triggerHotspot() {
-  var hotspot = spawn("hotspot", []);
+  var hotspot = spawn("hotspot")
 
   hotspot.stdout.on("data", data => {
-      console.log(`stdout: ${data}`);
-  });
+    console.log(`stdout: ${data}`)
+  })
 
   hotspot.stderr.on("data", data => {
-      console.log(`stderr: ${data}`);
-  });
+    console.log(`stderr: ${data}`)
+  })
 
   hotspot.on('error', (error) => {
-      console.log(`error: ${error.message}`);
-  });
+    console.log(`error: ${error.message}`)
+  })
 
   hotspot.on("close", code => {
-      console.log(`child process exited with code ${code}`);
-  });
+    console.log(`child process exited with code ${code}`)
+  })
 }
 
 async function configureInputDevice() {
